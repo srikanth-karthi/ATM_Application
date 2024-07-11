04446ad081c9b7094b209bf015199b3764f4f480
shrish <mynameisshrish@gmail.com>
2024-07-11 12:33:09 +0530

    Front End

---
+++ FrontEnd/js/global.js
@@ -0,0 +1,37 @@
+function addNumber(number) {
+    const input = document.getElementById('numberInput');
+    if(number=='00'&&input.value.length==0){
+        return
+    }
+    if(number=='00'&&input.value.length==5){
+        return 
+    }
+    if(input.value.length!=6){
+        input.value += number;
+    }
+}
+
+function cancel() {
+    const input = document.getElementById('numberInput');
+    input.value = '';
+}
+function clearInput(){
+    const input = document.getElementById('numberInput');
+    if (input.value.length > 0) {
+        input.value = input.value.slice(0, -1);
+    }
+
+}
+function enter() {
+    const input = document.getElementById('numberInput');
+    if (input.value.length != 6) {
+        alert("Enter Valid Card Number")
+        errorMessage.classList.remove('hidden');
+    }
+    else{
+        alert('Entered number: ' + input.value);
+        window.location.href="login.html"
+        input.value = '';
+    }
+    }
+    
