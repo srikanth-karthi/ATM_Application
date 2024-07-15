3a88b58637819582907ad041e75880952ff2c454
Shrish <mynameisshrish@gmail.com>
2024-07-15 07:54:43 +0530

    Final Commit

--- Backend/ATMMiniproject/ATMMiniproject/obj/Debug/net6.0/ATMMiniproject.AssemblyInfo.cs
+++ Backend/ATMMiniproject/ATMMiniproject/obj/Debug/net6.0/ATMMiniproject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ATMMiniproject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+167534fb853acd108ec89fb17f6f1474fde79cc4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a791c89347ad74e700980e857afbbef0df9c0ea2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ATMMiniproject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ATMMiniproject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ FrontEnd/Reame.md
@@ -0,0 +1,5 @@
+ATM Model Application
+
+The backend API Endpoints and the schema was designed by the backend team consisting of Sai Abiram, Srikanth and Neha.
+
+The frontend UI and integration was implemented by Shrish and Anthony.
